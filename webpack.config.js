2bd504f4d15f9813323df14300be705ce891348c
MC Curdy, Jeremiah (ITCDEC) - KLM <jeremiah.mccurdy@gmail.com>
2019-07-30 13:07:46 +0200

    rename index.jsx to js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.jsx'],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'bundle.[hash].js'
